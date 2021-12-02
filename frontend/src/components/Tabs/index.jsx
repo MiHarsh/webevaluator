025e72fd577b359136c94ee975f6e54faba4ed66
MiHarsh <harshm17172612@gmail.com>
2021-12-02 23:33:47 +0530

    WIP reportpage table

---
+++ frontend/src/components/Tabs/index.jsx
@@ -0,0 +1,93 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
+import Table from "pages/Table";
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`scrollable-auto-tabpanel-${index}`}
+      aria-labelledby={`scrollable-auto-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index) {
+  return {
+    id: `scrollable-auto-tab-${index}`,
+    "aria-controls": `scrollable-auto-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    width: "100%",
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
+const titles = [
+  "MediaURL",
+  "SiteURL",
+  "SSL Certificates",
+  "Cookies-I",
+  "Cookies-A",
+  "Cookies-D",
+  "WCAG2A",
+  "WCAG2AA",
+  "WCAG2AAA",
+  "Section 508",
+  "Error",
+  "Warnings",
+];
+
+export default function ScrollableTabs() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" color="default">
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          indicatorColor="primary"
+          textColor="primary"
+          variant="scrollable"
+          scrollButtons="auto"
+          aria-label="scrollable auto tabs example"
+        >
+          {titles.map((title, i) => (
+            <Tab label={title} {...a11yProps(i)} />
+          ))}
+        </Tabs>
+      </AppBar>
+
+      {titles.map((title, i) => (
+        <TabPanel value={value} index={i}>
+          <Table />
+        </TabPanel>
+      ))}
+    </div>
+  );
+}
